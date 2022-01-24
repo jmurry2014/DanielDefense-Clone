14c2a9d369c451dfddeb8a128380c7523e33ac7a
James Murry <jamesmurry@Jamess-Air.midcoip.net>
2022-01-23 22:58:33 -0600

    more frontend styling

--- danieldefense/src/components/main.js
+++ danieldefense/src/components/main.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import aside1 from '../images/aside1.jpg'
 import aside2 from '../images/aside2.jpg'
+import accessories from '../images/accessories.jpg'
+import plus from '../images/plus.svg'
+
 import pdw from '../images/pdw.jpg'
 function content(){
 return(
@@ -8,10 +11,57 @@ return(
 <header>
 <img src={pdw} alt=""/>
 </header>
-<aside>
-<img src={aside1} alt=""/>
-<img src={aside2} alt=""/>
-</aside>
+<section>
+    <div className="container">
+
+    <div className="promo-wrapper">
+        <img src={aside1} alt=""/>
+        <div className="promo-banner">
+            <h2>NO TRANSFER FEE</h2>
+        <p>Select a Dealer with “Preferred” by Their Name at Checkout.</p>
+        <button>Learn More</button>      
+        </div>   
+  </div>
+
+  <div className="promo-wrapper">
+        <img src={aside2} alt=""/>
+        <div className="promo-banner">
+            <h2>END OF YEAR SALE</h2>
+        <p>25% OFF OUTERWEAR & HOODIES</p>
+        <button>Learn More</button>      
+
+        </div>
+        
+  </div>
+
+
+
+    </div>
+ 
+    
+</section>
+   <section className="accessories">
+       <div className="wrapper">
+       <img src={accessories} alt="" />
+       <div className="accessories-btn">
+           <span>Accessories</span>
+       </div>
+       </div>
+   </section>
+   
+
+<section>
+    <div className="tester">het</div>
+</section>
+
+
+
+
+
+
+
+
+
     </div>   
 
 
